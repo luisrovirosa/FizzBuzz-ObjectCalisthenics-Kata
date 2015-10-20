99a015c09799da66c33ca9abdf0a26d8d1f4fab9
Luis Rovirosa <luisrovirosa@gmail.com>
2015-10-20 20:11:30 +0200

    FizzBuzzTest refactor: answerOf

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -6,9 +6,18 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function should_return_1_for_the_number_1()
     {
+        $this->assertEquals(new Answer('1'), $this->answerOf(1));
+    }
+
+    /**
+     * @param $number
+     * @return Answer
+     */
+    protected function answerOf($number)
+    {
         $fizzBuzz = new FizzBuzz();
         $solution = $fizzBuzz->execute();
 
-        $this->assertEquals(new Answer('1'), $solution->answer(new FizzNumber(1)));
+        return $solution->answer(new FizzNumber($number));
     }
 }
