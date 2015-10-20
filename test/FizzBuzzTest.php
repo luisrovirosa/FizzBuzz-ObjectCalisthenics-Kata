3c2e55cfd0462e34f1dab2895dcf4c826ac44b6b
Luis Rovirosa <luisrovirosa@gmail.com>
2015-10-20 20:12:09 +0200

    FizzBuzzTest refactor: assertExpectedAnswer

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -6,7 +6,16 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function should_return_1_for_the_number_1()
     {
-        $this->assertEquals(new Answer('1'), $this->answerOf(1));
+        $this->assertExpectedAnswer('1', 1);
+    }
+
+    /**
+     * @param $expected
+     * @param $number
+     */
+    protected function assertExpectedAnswer($expected, $number)
+    {
+        $this->assertEquals(new Answer($expected), $this->answerOf($number));
     }
 
     /**
